fd00fcb288978150cf6cf59bf8d75c85e3f89dfb
spicysoda <49571907+spicysoda@users.noreply.github.com>
2019-04-21 15:40:47 +0800

    Update cash.c

--- pset1/cash.c
+++ pset1/cash.c
@@ -9,7 +9,7 @@ int main(void)
     float change;
     do
     {
-    change = get_float("What is the change?\n");
+    change = get_float("What is the change?\n");          // user input
     }
     while (change < 0);
         
@@ -21,25 +21,25 @@ void coins(float change)
     int change_cents = round(change*100); // convert change to cents
     int coins = 0;
     
-    while (change_cents >= 25)
-    {
+    while (change_cents >= 25)          // when amount is more than 25, divide by 25. Add no. of division of amt of coins
+    {                                   // when amount less than 25, move to next loop
         change_cents -= 25;
         coins++;
     }
     
-    while (change_cents >= 10)
+    while (change_cents >= 10)          // 10 cents
     {
         change_cents -= 10;
         coins++;
     }
     
-        while (change_cents >= 5)
+        while (change_cents >= 5)       // 5 cents
     {
         change_cents -= 5;
         coins++;
     }
     
-        while (change_cents >= 1)
+        while (change_cents >= 1)       // 1 cent
     {
         change_cents -= 1;
         coins++;
