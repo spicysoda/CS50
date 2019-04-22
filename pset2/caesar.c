1c55f1a2b204ea08eca91413a032be6d24dcc8a4
spicysoda <49571907+spicysoda@users.noreply.github.com>
2019-04-22 23:56:14 +0800

    Update caesar.c

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -4,60 +4,55 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int main(int argc, string argv[]) {         // check for 2 arg 
-   
-     if (argc != 2) {
+int main(int argc, string argv[])
+{ 
+    if (argc != 2)
+    {
         printf("Usage: ./caesar key");
         
     return 1;
     }
     
-    int number = atoi(argv[1]); 
+    int key = atoi(argv[1]);
         
-    if (number <= 0) {                     // check for number & positive
+        if (key <= 0)
+    {
         printf("Usage: ./caesar key");
         
     return 1;
     }    
     
-// user input  
-  
-    string input = get_string("Input:\n");
+    string input = get_string("Input:\n"); // input
     printf("ciphertext: "); // output
-    int key = number;
-
-// keeps key less than 26
-    while (key > 26) {
-        key -=26;
-    }  
-
-// cipher each char
-  
+      
     for (int i = 0, n = strlen(input); i < n; i++ ) {
  
-            if (isalpha(input[i])) {
-
-// brings char back to first of alphabet by minusing total alphabets (26). 
-              
-                if ((isupper(input[i]) && (input[i] + key > 'Z')) || (islower(input[i]) && (input[i] + key > 'z'))) {
-                    int output = (input[i] + key) - 26;
-                    printf("%c", output);
-                }
-
-                else {
-                    int output =  input[i] + key;
-                    printf("%c", output);
-                }
+        if (isalpha(input[i])) {
+                
+            if (isupper(input[i])) {
+                input[i] = ((input[i] + key) - 'A') % 26;
+                printf("%c", input[i] + 'A');
             }
                 
+            else if (islower(input[i])) {
+                input[i] = ((input[i] + key) - 'a') % 26;
+                printf("%c", input[i] + 'a');
+            }  
+                     
             else {
-                printf("%c", input[i]);
+                int output =  input[i] + key;
+                printf("%c", output);
             }
-    } 
-    
+        }
+        
+        else {
+            printf("%c", input[i]);         
+         }
+    }
+ 
     printf("\n");
-            
     return 0;    
+        
 }
 
 
